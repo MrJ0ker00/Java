dfb247da2ff1394fd436234172bd1b08f3cd6a2d
J0ker00 <asdmmih@gmail.com>
2023-11-02 21:15:09 +0200

    first commitHomework

---
+++ Task1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Task1{
+    public static void main(String[] args) {
+        int alex = 7;
+        int maks = 9;
+        maks++;
+        System.out.println("Сумма равна: " + (alex + maks));
+    }
+}
---
+++ Task3.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        System.out.println("It's Java string: C:\\Program Files\\Java\\jdk1.7.0\\bin\\\"");
+    }
+}
